407a3e42dd229e847ad614dd1de5e2d9c4285113
gucchi0421 <gucchi.1999.1121.0421@gmail.com>
2025-01-07 16:47:24 +0900

    初回
    react-router v7のSSGでセットアップ

---
+++ app/routes.ts
@@ -0,0 +1,8 @@
+import { type RouteConfig, index, route } from '@react-router/dev/routes'
+
+export default [
+  index('routes/home/home.tsx'),
+  // route('/contact', 'routes/contact/index.tsx'),
+  // route('/about', 'routes/about.tsx'),
+  // route('/about', 'routes/about.tsx'),
+] satisfies RouteConfig
